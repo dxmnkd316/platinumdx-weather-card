42f2605dd10f092a8314e0e01302f23f64e33de9
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2023-09-26 11:48:05 -0500

    Update editor.ts

--- src/editor.ts
+++ src/editor.ts
@@ -14,7 +14,7 @@ import { selectDefinition } from '../elements/select';
 import { switchDefinition } from '../elements/switch';
 import { textfieldDefinition } from '../elements/textfield';
 
-@customElement('platinum-weather-card-editor')
+@customElement('platinumdx-weather-card-editor')
 export class WeatherCardEditor extends LitElement implements LovelaceCardEditor {
   @property({ attribute: false }) public hass?: HomeAssistant;
 
