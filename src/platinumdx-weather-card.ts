893c2e43130ef13f5093f6f175314cc579d1d1aa
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2024-05-06 14:26:48 -0500

    CSS modifications to wrap text in tooltips

--- src/platinumdx-weather-card.ts
+++ src/platinumdx-weather-card.ts
@@ -2615,6 +2615,7 @@ export class PlatinumdxWeatherCard extends LitElement {
       .fcasttooltiptext {
         padding-left: 8px;
         padding-right: 8px;
+        white-space: pre-wrap;
       }
     `;
   }
